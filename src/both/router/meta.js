4c7210c3dee07a7e974be620510cf5c0600ca4f1
junhaoyap <lordyyap@gmail.com>
2015-07-12 18:14:06 +0800

    Changed project title to Pioneer Alumni Circle

--- src/both/router/meta.js
+++ src/both/router/meta.js
@@ -1,9 +1,9 @@
-if(Meteor.isClient) {
+if (Meteor.isClient) {
   Meta.config({
       options: {
         // Meteor.settings[Meteor.settings.environment].public.meta.title
-        title: 'Default Project Title',
-        suffix: 'Project Title'
+        title: 'Pioneer Alumni Circle',
+        suffix: 'PAC'
       }
   });
 }
